a411cfc4c9c85bac49df91b007b1e9407a0f6f4d
HannahChukwu <hannahchukwu03@gmail.com>
2024-10-17 22:30:41 -0400

    I did all the changes

--- src/main/kotlin/edu/trincoll/hr/Employee.kt
+++ src/main/kotlin/edu/trincoll/hr/Employee.kt
@@ -1,5 +1,6 @@
 package edu.trincoll.hr
 
+
 // Abstract class Employee should have:
 //   - a name of type String
 //   - an id of type Int.
@@ -11,6 +12,23 @@ package edu.trincoll.hr
 //
 // It should override the toString method to
 // return a string with the name and id of the employee.
-abstract class Employee(
-) {
+abstract class Employee(val name: String, val id: Int) : Comparable<Employee>{
+    abstract fun pay(): Double
+
+    override fun compareTo(other: Employee): Int {
+
+        val payCompare = this.pay().compareTo(other.pay())
+        if (payCompare != 0) return payCompare
+
+        val nameCompare = this.name.compareTo(other.name)
+        if (nameCompare != 0) return nameCompare
+
+        return this.id.compareTo(other.id)
+
+    }
+
+    override fun toString(): String {
+        return "Employee(name=$name, id=$id"
+    }
+
 }
--- src/main/kotlin/edu/trincoll/hr/HR.kt
+++ src/main/kotlin/edu/trincoll/hr/HR.kt
@@ -6,5 +6,31 @@ package edu.trincoll.hr
 //   - a fire method that takes an id and returns a new HR object with the employee with that id removed
 //   - a payEmployees method that returns the total pay of all employees
 class HR(private val employees: List<Employee> = emptyList()) {
+    fun hire(employee: Employee) : HR {
+        val newEmployees = employees + employee
+        return HR(newEmployees)
+    }
+    fun fire(id: Int) : HR{
+        val newEmployees = mutableListOf<Employee>()
+        for(employee in employees){
+            if(employee.id != id){
+                newEmployees.add(employee)
+            }
+        }
+        return HR(newEmployees)
+
+    }
+
+    fun payEmployees() : Double{
+        var totalPay = 0.0
+        for(employee in employees){
+            totalPay += employee.pay()
+        }
+        return totalPay
+
+    }
+
 
 }
+
+
